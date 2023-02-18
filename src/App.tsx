a8c587e658b65a66d9942bcfdff3b2f0ca3b2b09
geoflemaire <124359952+geoflemaire@users.noreply.github.com>
2023-02-18 19:50:04 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Geoffrey LEMAIRE</h1>
+      <p>hypnothérapie</p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/ziPzOp6j0Oo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    </div>
+  );
+}
+
+export default App;
